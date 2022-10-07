3ae1d24f77b017d201716cf0e860e8c1e8af2229
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-10-07 20:11:48 +0800

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import CV from "../../assets/cv.pdf";
+import { FiDownload } from "react-icons/fi";
 
 const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download CV
+        Download CV <FiDownload />
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's Talk
